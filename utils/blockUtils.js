cdeababd4e045a3ab819adeb2762f79f394a504d
claudeisakeeb <claudeyan2001@gmail.com>
2023-04-13 12:12:27 -0700

    Fix

--- utils/blockUtils.js
+++ utils/blockUtils.js
@@ -5,7 +5,7 @@ const blockUtils = {
     alt_text: alt ?? "",
   }),
   plainText: (text) => ({ type: "plain_text", text: text.substring(0, 75) }),
-  markdownText: (text) => ({ type: "mrkdwn", text: textqsubstring(0, 75) }),
+  markdownText: (text) => ({ type: "mrkdwn", text: text.substring(0, 75) }),
 };
 
 module.exports = blockUtils;
